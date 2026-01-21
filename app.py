f7b59b9892c8c721621d1d96bea44d3e8e343959
nyaniTz <cliffmlimwa@gmail.com>
2026-01-21 17:33:55 +0200

    Remove swallow_errors option from app initialization
    Removed swallow_errors option to handle redis errors.

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ limiter = Limiter(
     app=app,
     default_limits=["60 per minute"],
     storage_uri=os.getenv("REDIS_URL", "memory://"),
-    swallow_errors=True,   # prevents 500 if redis drops
+
 )
 
 
